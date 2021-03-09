05390580aa762593c9e896ae8966f52811802c64
Noel Injaga <injaganoel@gmail.com>
2021-03-09 16:47:06 +0300

    added a readme file

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function PizzaCost(toppings, size){
   this.toppings = toppings,
   this.size = size
 }
-PizzaCost.prototype.toppingsBasedAmount = function(){
+PizzaCost.prototype.toppingsCost = function(){
     if(this.toppings.length != 0){
       var toppingPrice = this.toppings.length *3;
       return toppingPrice;
@@ -11,7 +11,7 @@ PizzaCost.prototype.toppingsBasedAmount = function(){
     }
   }
 
-  PizzaCost.prototype.sizeBasedAmount = function(){
+  PizzaCost.prototype.sizeCost = function(){
   if (this.size === "Small"){
     return 4;
   } else if(this.size === "Medium"){
@@ -48,11 +48,38 @@ $(document).ready(function(){
 
    PizzaCost = new PizzaCost(toppingArray,inputtedSize);
 
-   var toppingBasedAmt = PizzaCost.toppingsBasedAmount();
-   var sizeBasedAmt = PizzaCost.sizeBasedAmount();
+   var toppingBasedAmt = PizzaCost.toppingsCost();
+   var sizeBasedAmt = PizzaCost.sizeCost();
    PizzaCost.calculateCost(toppingBasedAmt, sizeBasedAmt);
   });
 });
 $("#address1").click(function(){
   alert("We are happy to serve you to your preferred location, This attracts a delivery Charge of Ksh. 100.00");
     });
+
+    $('.item1').click(function(){
+      $(".col-md-4").toggle();
+      $("descr").toggle();
+    });
+    $('.item2').click(function(){
+      $("#c1,#c2,#c3").toggle();
+    });
+    $('.item3').click(function(){
+      $("#t1,#t2,#t3,#t4,#t5,#t6,#t7").toggle();
+    });
+    $('.item4').click(function(){
+      $("#col-md-3").toggle();
+    });
+    $('#men').click(function(){
+      $(".card-menu").slideDown();
+    });
+    $('#sz').click(function(){
+      $(".card-sizes").slideDown();
+    });
+    $('#prz').click(function(){
+      $(".card-crusts,.card-toppings").toggle();
+    });
+    $('#active').click(function(){
+      $(".card-menu,.card-crusts,.card-toppings,.item4,.card-sizes,.card-prices").toggle();
+    });
+    
